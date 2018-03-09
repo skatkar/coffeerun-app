214d25462347bbf051f287fe5864c1eba5df5247
skatkar <skatkar225@gmail.com>
2018-03-09 12:20:19 -0800

    Deployd service call

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -1,41 +1,65 @@
-(function(window){
+(function(window) {
   "use strict";
   var App = window.App || {};
   var $ = window.jQuery;
 
-  function RemoteDataStore(url){
-    if(!url){
+  function RemoteDataStore(url) {
+    if (!url) {
       throw new Error("URL not provided");
     }
 
     this.serverUrl = url;
   }
 
-  RemoteDataStore.prototype.add = function(key,val){
-    $.post(this.serverUrl,val,function(serverResponse){
+  RemoteDataStore.prototype.add = function(key, val) {
+    this.remove(key);
+    $.post(this.serverUrl, val, function(serverResponse) {
       console.log(serverResponse);
     });
   };
 
-  RemoteDataStore.prototype.getAll = function(cb){
-    $.get(this.serverUrl,function(serverResponse){
+  RemoteDataStore.prototype.getAll = function(cb) {
+    $.get(this.serverUrl, function(serverResponse) {
       console.log(serverResponse);
       cb(serverResponse);
     });
   };
 
-  RemoteDataStore.prototype.get = function(key,cb){
+  /*RemoteDataStore.prototype.get = function(key,cb){
     $.get(this.serverUrl + "/" + key ,function(serverResponse){
       console.log(serverResponse);
       cb(serverResponse);
     });
-  };
+  };*/
 
-  RemoteDataStore.prototype.remove = function(key){
-    $.ajax(this.serverUrl + "/" + key ,{
-      type: "DELETE"
+  RemoteDataStore.prototype.get = function(key, cb) {
+    $.get(this.serverUrl + "?emailAddress=" + key, function(serverResponse) {
+      console.log(serverResponse);
+      cb(serverResponse);
     });
   };
+
+  RemoteDataStore.prototype.remove = function(key) {
+    var serverUrl = this.serverUrl;
+    this.get(key, function(serverResponse) {
+      var id = getId(key, serverResponse[0]["id"]);
+
+      $.ajax(serverUrl + "/" + id ,{
+        type: "DELETE"
+      });
+    });
+  };
+
+  function getId(key, id) {
+    console.log("The id to remove is :" + id);
+    return id;
+  }
+
+  /*RemoteDataStore.prototype.remove = function(key){
+    $.ajax(this.serverUrl + "?emailAddress=" + key ,{
+      type: "DELETE"
+    });
+  };*/
 
   App.RemoteDataStore = RemoteDataStore;
   window.App = App;
